c9b87560db30761f9047a39b12dc7024fb385426
urbanKNS <angelknsrg@gmail.com>
2023-12-06 02:52:34 -0500

    se agrega el proyecto de software git software.py

---
+++ git software.py
@@ -0,0 +1,5 @@
+# ¡Bienvenido a mi Repositorio!
+
+print ("¡Hola a todos! Somos el Grupo de venta de voletos de transporte, y estomos emocionado de compartir nuestro software en este repositorio.")
+
+
